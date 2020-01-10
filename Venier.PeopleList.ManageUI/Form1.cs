46ce44bce497d922ecedbcedeb25ec0694e82732
francescavenier <francescavenier10@gmail.com>
2020-01-10 10:56:30 +0100

    Insert message in queue

--- Venier.PeopleList.ManageUI/Form1.cs
+++ Venier.PeopleList.ManageUI/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,21 +10,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Venier.PeopleList.Data;
 
 namespace Venier.PeopleList.ManageUI
 {
     public partial class Form1 : Form
     {
+        public CloudQueue Queue { get; set; }
         public Form1()
         {
             InitializeComponent();
-            // Verifica esistenza coda
+            // Connection to Azure
+            Auth auth = new Auth();
+            var storageAccount = CloudStorageAccount.Parse(auth.connectionString);
+            var queueClient = storageAccount.CreateCloudQueueClient();
 
+            Queue = queueClient.GetQueueReference("myqueue");
+            Queue.CreateIfNotExists();
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
+            People user = new People();
+            user.name = txtName.Text;
+            user.surname = txtSurname.Text;
+            user.birthDate = DateTime.Parse(txtBirthDate.Text);
+            user.address = txtAddress.Text;
 
+             //Serialize message
+            var JsonMessage = JsonConvert.SerializeObject(user);
+
+            //Insert message in queue
+            CloudQueueMessage queueMessage = new CloudQueueMessage(JsonMessage);
+            Queue.AddMessage(queueMessage);
         }
     }
 }
